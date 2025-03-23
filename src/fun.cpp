9c434c41a8ecdfb14f674c216af47c6cca209303
TriggeredRKr <Triggeredzj@gmail.com>
2025-03-23 20:31:47 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,99 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    const char* c = str;
+    bool word = false;
+    bool correct = true;
+    while (*c != '\0')
+    {
+        if (isspace(*c))
+        {
+            if (word && correct)
+            {
+                count++;
+            }
+            word = false;
+            correct = true;
+        }
+        else if (isdigit(*c))
+        {
+            word = true;
+            correct = false;
+        }
+        else if (isalpha(*c))
+        {
+            word = true;
+        }
+        c++;
+    }
+    if (word && correct)
+    {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int count = 0;
+    const char* c = str;
+    bool word = false;
+    bool correct = true;
+    while (*c != '\0')
+    {
+        if (isupper(*c) && word == false)
+        {
+            correct = true;
+            word = true;
+        }
+        else if (word == true && !islower(*c) && !isspace(*c))
+        {
+            correct = false;
+        }
+
+        else if (isspace(*c))
+        {
+            if (correct && word)
+            {
+                count++;
+            }
+            word = false;
+            correct = false;
+        }
+        c++;
+    }
+    if (word && correct)
+    {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    int count = 0;
+    int length = 0;
+    bool word = false;
+    const char* c = str;
+    while (*c != '\0')
+    {
+        if (!isspace(*c))
+        {
+            word = true;
+            length++;
+        }
+        else
+        {
+            if (word)
+            {
+                count++;
+            }
+            word = false;
+        }
+        c++;
+    }
+    if (count != 0)
+    {
+        return (round((double)length / count));
+    }
+    else return 0;
 }
