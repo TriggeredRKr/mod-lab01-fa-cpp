2eb6342e1f5fd7842dfdfc057446df69341ef247
TriggeredRKr <Triggeredzj@gmail.com>
2025-03-23 20:58:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 
 int main() {
     const char* str1 = "  One     2to Thr3 Four five 6ix  \n";
-    printf("String:  %s", str1);
-    printf("faStr1 : %d\n", faStr1(str1));
-    printf("faStr2 : %d\n", faStr2(str1));
-    printf("faStr3 : %d", faStr3(str1));
+    std::cout << "Строка: "<< str1 << std::endl;
+    std::cout << "faStr1 : " << faStr1(str1) << std::endl;
+    std::cout << "faStr2 : " << faStr2(str1) << std::endl;
+    std::cout << "faStr3 : " << faStr3(str1) << std::endl;
     return 0;
 }
