bc1d7a44c596812657347280da68f66f7280b794
TriggeredRKr <Triggeredzj@gmail.com>
2025-03-23 20:36:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* str1 = "  One     2to Thr3 Four five 6ix  \n";
+    printf("String:  %s", str1);
+    printf("faStr1 : %d\n", faStr1(str1));
+    printf("faStr2 : %d\n", faStr2(str1));
+    printf("faStr3 : %d", faStr3(str1));
     return 0;
 }
